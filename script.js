3dc275df463620caa3696933ffdb749f0d235532
xXQueenLoloshXx <alaawalhamid@gmail.com>
2021-05-31 17:50:10 +0300

    Created a random Hero

---
+++ script.js
@@ -0,0 +1,19 @@
+let ranDomNumber = num => {
+    return Math.floor(Math.random() * num);
+ }
+ let SuperHeroTrates = {
+   gender : ['Male','Female'],
+   power: ['Fire', 'Visibility','Healing','Flying'],
+   city: ['Gotham','Hogwards','Narnia','Disney'],
+   animal: ['cat','bat','dragon','dog'],
+ 
+   createHero(){
+     let nGender = this.gender[ranDomNumber(this.gender.length)];
+     let nPower = this.power[ranDomNumber(this.power.length)];
+     let nCity = this.city[ranDomNumber(this.city.length)];
+     let nAnimal = this.animal[ranDomNumber(this.animal.length)];
+     console.log(`your a ${nGender} superhero, your power will be the power of ${nPower},  \nthe city you will protect is ${nCity} city , and your sidekick is a ${nAnimal}.`);
+   }
+ }
+ 
+ console.log(SuperHeroTrates.createHero());
